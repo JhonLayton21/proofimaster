b295da512c50cd9535c27aee47a542129daddc51
Jhon <laytonjhon0@gmail.com>
2024-06-02 17:19:01 -0500

    configuracion inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import proofisillasLogo from '/proofisillas.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div className="bg-slate-100 dark:bg-slate-800">
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={proofisillasLogo} className="logo" alt="Proofisillas logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1 className="text-5xl not-italic font-bold">Bienvenido a PROOFIMASTER</h1>
+        <h2 className="text-2xl italic font-bold">Gestiona tu negocio fácilmente y enfocate en la productividad</h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
