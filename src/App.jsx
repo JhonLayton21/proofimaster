7282182e1247fb23ea56e49f4f77f58b0b27186b
Jhon <laytonjhon0@gmail.com>
2024-06-04 18:55:49 -0500

    recambio de titulo

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src={proofisillasLogo} className="logo" alt="Proofisillas logo" />
         </a>
-        <h1 className="text-5xl not-italic font-bold">NEGRO CHUPON</h1>
+        <h1 className="text-5xl not-italic font-bold">BIENVENIDO A PROOFIMASTER</h1>
         <h2 className="text-2xl italic font-bold">Gestiona tu negocio fácilmente y enfocate en la productividad</h2>
       </div>
     </>
