8ce3aea01902d8b749bf366d2c47c2b73a4c5fae
Jhon <laytonjhon0@gmail.com>
2024-06-04 19:55:05 -0500

    prueba deploy con github

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,9 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src={proofisillasLogo} className="logo" alt="Proofisillas logo" />
         </a>
-        <h1 className="text-5xl not-italic font-bold">BIENVENIDO A PROOFIMASTER</h1>
+        <h1 className="text-5xl not-italic font-bold">BIENVENIDO A PROOFIMASTER!</h1>
         <h2 className="text-2xl italic font-bold">Gestiona tu negocio fácilmente y enfocate en la productividad</h2>
+        <h3>texto para probar deploy con github</h3>
       </div>
     </>
   )
