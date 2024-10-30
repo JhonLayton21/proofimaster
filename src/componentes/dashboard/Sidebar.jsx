cb942aad1627f2d4761ea6e857e5fd1b1669aaf2
Esteban <juan.garzonb@uniagustiniana.edu.co>
2024-10-29 19:41:50 -0500

    Arreglo menú lateral

--- src/componentes/dashboard/Sidebar.jsx
+++ src/componentes/dashboard/Sidebar.jsx
@@ -1,13 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAddressBook, faList, faCartShopping, faChartSimple, faHouse, faMoneyCheckDollar, faRightFromBracket, faTruckFast } from '@fortawesome/free-solid-svg-icons';
 import { Link, useNavigate } from "react-router-dom";
 import { ChevronDownIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
 import { Accordion, AccordionHeader, AccordionBody, List, ListItem, ListItemPrefix, Drawer, Card, Typography } from "@material-tailwind/react";
-import { getAuth, signOut } from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 import appFirebase from '../../credenciales';
 import { supabase } from '../../../supabase';
-
 
 const Sidebar = ({ isDrawerOpen, closeDrawer, open, handleOpen, isActive }) => {
     const auth = getAuth(appFirebase);
@@ -15,11 +14,9 @@ const Sidebar = ({ isDrawerOpen, closeDrawer, open, handleOpen, isActive }) => {
 
     const logout = async () => {
         const { error } = await supabase.auth.signOut();
-
         if (error) {
             console.log(error);
         }
-
         navigate("/login");
     };
 
@@ -31,7 +28,7 @@ const Sidebar = ({ isDrawerOpen, closeDrawer, open, handleOpen, isActive }) => {
                 className="h-[calc(100vh-2rem)] w-full p-4"
             >
                 {/* HEADER */}
-                <div className="mb-2  block p-4 border-b border-solid">
+                <div className="mb-2 block p-4 border-b border-solid">
                     <h1 className="text-left text-slate-100 hover:text-orange-800 md:text-xl lg:text-2xl font-bold whitespace-nowrap overflow-hidden">
                         <Link to="/">PROOFIMASTER</Link>
                     </h1>
@@ -42,33 +39,42 @@ const Sidebar = ({ isDrawerOpen, closeDrawer, open, handleOpen, isActive }) => {
                 {/* MENU */}
                 <div className="flex items-center justify-start flex-grow">
                     <ul className="list-none">
-                        <li className={`menu-item text-left font-normal text-2xl ${isActive('/')}`}>
-                            <FontAwesomeIcon icon={faHouse} className="fa-1x mr-4" />
-                            <Link to="/">Dashboard</Link>
-                        </li>
-                        <li className={`menu-item text-left font-normal text-2xl ${isActive('/productos')}`}>
-                            <FontAwesomeIcon icon={faCartShopping} className="fa-1x mr-4" />
-                            <Link to="/productos">Productos</Link>
-                        </li>
-                        <li className={`menu-item text-left font-normal text-2xl ${isActive('/ventas')}`}>
-                            <FontAwesomeIcon icon={faMoneyCheckDollar} className="fa-1x mr-4" />
-                            <Link to="/ventas">Ventas</Link>
-                        </li>
-                        <li className={`menu-item text-left font-normal text-2xl ${isActive('/proveedores')}`}>
-                            <FontAwesomeIcon icon={faTruckFast} className="fa-1x mr-4" />
-                            <Link to="/proveedores">Proveedores</Link>
-                        </li>
-                        <li className={`menu-item text-left font-normal text-2xl ${isActive('/clientes')}`}>
-                            <FontAwesomeIcon icon={faAddressBook} className="fa-1x mr-4" />
-                            <Link to="/clientes">Clientes</Link>
-                        </li>
+                        <Link to="/">
+                            <li className={`menu-item text-left font-normal text-2xl ${isActive('/')}`}>
+                                <FontAwesomeIcon icon={faHouse} className="fa-1x mr-4" />
+                                Dashboard
+                            </li>
+                        </Link>
+                        <Link to="/productos">
+                            <li className={`menu-item text-left font-normal text-2xl ${isActive('/productos')}`}>
+                                <FontAwesomeIcon icon={faCartShopping} className="fa-1x mr-4" />
+                                Productos
+                            </li>
+                        </Link>
+                        <Link to="/ventas">
+                            <li className={`menu-item text-left font-normal text-2xl ${isActive('/ventas')}`}>
+                                <FontAwesomeIcon icon={faMoneyCheckDollar} className="fa-1x mr-4" />
+                                Ventas
+                            </li>
+                        </Link>
+                        <Link to="/proveedores">
+                            <li className={`menu-item text-left font-normal text-2xl ${isActive('/proveedores')}`}>
+                                <FontAwesomeIcon icon={faTruckFast} className="fa-1x mr-4" />
+                                Proveedores
+                            </li>
+                        </Link>
+                        <Link to="/clientes">
+                            <li className={`menu-item text-left font-normal text-2xl ${isActive('/clientes')}`}>
+                                <FontAwesomeIcon icon={faAddressBook} className="fa-1x mr-4" />
+                                Clientes
+                            </li>
+                        </Link>
                         <Accordion
                             open={open === 1}
                             icon={
                                 <ChevronDownIcon
                                     strokeWidth={2.5}
-                                    className={`mx-auto h-4 w-4 transition-transform ${open === 1 ? "rotate-180" : ""
-                                        }`}
+                                    className={`mx-auto h-4 w-4 transition-transform ${open === 1 ? "rotate-180" : ""}`}
                                 />
                             }
                         >
@@ -84,53 +90,66 @@ const Sidebar = ({ isDrawerOpen, closeDrawer, open, handleOpen, isActive }) => {
                                 </AccordionHeader>
                             </ListItem>
                             <AccordionBody className="py-1">
-                                <List className="p-0 text-slate-100 ">
-                                    <ListItem className="sub-item">
-                                        <ListItemPrefix>
-                                            <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
-                                        </ListItemPrefix>
-                                        <Link to="/metodos-pago">Métodos de pago</Link>
-                                    </ListItem>
-                                    <ListItem className="sub-item">
-                                        <ListItemPrefix>
-                                            <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
-                                        </ListItemPrefix>
-                                        <Link to="/marca-productos">Marca productos</Link>
-                                    </ListItem>
-                                    <ListItem className="sub-item">
-                                        <ListItemPrefix>
-                                            <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
-                                        </ListItemPrefix>
-                                        <Link to="/referencia-productos">Referencia productos</Link>
-                                    </ListItem>
-                                    <ListItem className="sub-item">
-                                        <ListItemPrefix>
-                                            <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
-                                        </ListItemPrefix>
-                                        <Link to="/tipo-clientes">Tipo clientes</Link>
-                                    </ListItem>
-                                    <ListItem className="sub-item">
-                                        <ListItemPrefix>
-                                            <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
-                                        </ListItemPrefix>
-                                        <Link to="/estados-venta">Estados venta</Link>
-                                    </ListItem>
-                                    <ListItem className="sub-item">
-                                        <ListItemPrefix>
-                                            <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
-                                        </ListItemPrefix>
-                                        <Link to="/metodo-envio-venta">Metodo envio venta</Link>
-                                    </ListItem>
+                                <List className="p-0 text-slate-100">
+                                    <Link to="/metodos-pago">
+                                        <ListItem className="sub-item">
+                                            <ListItemPrefix>
+                                                <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                                            </ListItemPrefix>
+                                            Métodos de pago
+                                        </ListItem>
+                                    </Link>
+                                    <Link to="/marca-productos">
+                                        <ListItem className="sub-item">
+                                            <ListItemPrefix>
+                                                <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                                            </ListItemPrefix>
+                                            Marca productos
+                                        </ListItem>
+                                    </Link>
+                                    <Link to="/referencia-productos">
+                                        <ListItem className="sub-item">
+                                            <ListItemPrefix>
+                                                <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                                            </ListItemPrefix>
+                                            Referencia productos
+                                        </ListItem>
+                                    </Link>
+                                    <Link to="/tipo-clientes">
+                                        <ListItem className="sub-item">
+                                            <ListItemPrefix>
+                                                <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                                            </ListItemPrefix>
+                                            Tipo clientes
+                                        </ListItem>
+                                    </Link>
+                                    <Link to="/estados-venta">
+                                        <ListItem className="sub-item">
+                                            <ListItemPrefix>
+                                                <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                                            </ListItemPrefix>
+                                            Estados venta
+                                        </ListItem>
+                                    </Link>
+                                    <Link to="/metodo-envio-venta">
+                                        <ListItem className="sub-item">
+                                            <ListItemPrefix>
+                                                <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                                            </ListItemPrefix>
+                                            Metodo envio venta
+                                        </ListItem>
+                                    </Link>
                                 </List>
                             </AccordionBody>
                         </Accordion>
-                        <li className={`menu-item text-left font-normal text-2xl ${isActive('/informes')}`}>
-                            <FontAwesomeIcon icon={faChartSimple} className="fa-1x mr-4" />
-                            <Link to="/informes">Informes</Link>
-                        </li>
+                        <Link to="/informes">
+                            <li className={`menu-item text-left font-normal text-2xl ${isActive('/informes')}`}>
+                                <FontAwesomeIcon icon={faChartSimple} className="fa-1x mr-4" />
+                                Informes
+                            </li>
+                        </Link>
                     </ul>
                 </div>
-
 
                 {/* FOOTER */}
                 <div className="flex justify-start border-t border-solid mt-auto p-4">
